964771c6d89249e5ce846f992e5bb5978df1343a
Eren <vrsozluk@gmail.com>
2023-04-29 21:32:28 +0300

    pages are done

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+
+// Define routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
